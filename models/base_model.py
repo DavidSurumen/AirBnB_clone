c5deee440d471590bf7a37645aac56f89c9a9641
David Surumen <davidsurumen@gmail.com>
2022-12-30 12:26:52 +0300

    .

--- models/base_model.py
+++ models/base_model.py
@@ -9,17 +9,9 @@ class BaseModel():
 
     def __init__(self, *args, **kwargs):
         """constructor of a BaseModel."""
-        if kwargs is not None and kwargs != {}:
-            for key, val in kwargs.items():
-                if key == 'created_at' or \
-                        key == 'updated_at':
-                    self.__dict__[key] = datetime.fromisoformat(val)
-                else:
-                    self.__dict__[key] = val
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
         """Returns a human readable string repr of an instance."""
