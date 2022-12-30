82a05e1f751f8ed344dce65255cecafb13a86669
David Surumen <davidsurumen@gmail.com>
2022-12-30 12:38:57 +0300

    .

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,7 @@ class BaseModel:
         """Initialization of a Base instance."""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
         """Returns a human-readable string repr of an instance."""
