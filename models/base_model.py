b38cce1816935cd5f33338466876673a57e57e04
David Surumen <davidsurumen@gmail.com>
2023-01-02 16:59:43 +0300

    .

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """This module defines the class BaseModel."""
 import uuid
 from datetime import datetime
-#from models import storage
+from models import storage
 
 
 class BaseModel():
@@ -23,7 +23,6 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = datetime.utcnow()
-            from models import storage
             storage.new(self)
 
     def __str__(self):
@@ -35,7 +34,7 @@ class BaseModel():
     def save(self):
         """updates the public instance attribute 'updated_at' with the current
         datetime."""
-        from models import storage
+
         self.updated_at = datetime.utcnow()
         # storage.new(self)   # -> this does not really have to be there.
         storage.save()
