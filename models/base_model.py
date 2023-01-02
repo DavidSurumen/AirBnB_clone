ee2add6f86f53c5c8b00d53371dc44bbb80c70de
David Surumen <davidsurumen@gmail.com>
2023-01-02 21:13:47 +0300

    used aware datetime objects

--- models/base_model.py
+++ models/base_model.py
@@ -21,7 +21,7 @@ class BaseModel():
                     self.__dict__[key] = val
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
+            self.created_at = datetime.now()
             storage.new(self)
 
     def __str__(self):
@@ -33,7 +33,7 @@ class BaseModel():
     def save(self):
         """updates the public instance attribute 'updated_at' with the current
         datetime."""
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.now()
         storage.new(self)   # to add the attribute 'update_now', and any other
         storage.save()
 
