958e1e7904db51dc0f775ede6e355b8e80b546d0
David Surumen <davidsurumen@gmail.com>
2023-01-02 17:11:50 +0300

    .

--- models/base_model.py
+++ models/base_model.py
@@ -22,7 +22,6 @@ class BaseModel():
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
             storage.new(self)
 
     def __str__(self):
