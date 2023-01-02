b68aa423a03f79b73f405b50e96dc415f8e4d030
David Surumen <davidsurumen@gmail.com>
2023-01-02 12:23:27 +0300

    modif

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,61 +2,65 @@
 """Test module for base_model.py"""
 import unittest
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 import uuid
 from datetime import datetime
+import os
 
 
 class TestBaseModel(unittest.TestCase):
     """Implements test cases for class BaseModel."""
+    def setUp(self):
+        """setup code to be ran for every test."""
+
+        self.obj = BaseModel()  # for the tests that need
 
     def test_has_attributes(self):
         """Asserts that instance attributes are created."""
-        obj = BaseModel()
-        self.assertTrue(hasattr(obj, 'created_at'))
-        self.assertTrue(hasattr(obj, 'updated_at'))
-        self.assertTrue(hasattr(obj, 'id'))
+
+        self.assertTrue(hasattr(self.obj, 'created_at'))
+        self.assertTrue(hasattr(self.obj, 'updated_at'))
+        self.assertTrue(hasattr(self.obj, 'id'))
 
     def test_id_is_string(self):
         """Asserts that the attribute 'id' is converted to string."""
-        obj = BaseModel()
-        self.assertTrue(type(obj.id), str)
+
+        self.assertTrue(type(self.obj.id), str)
 
     def test_save(self):
         """Tests that 'updated_at' is not having the same
         value as 'created_at'."""
-        obj = BaseModel()
 
-        self.assertEqual(obj.created_at, obj.updated_at)
-        obj.save()
-        self.assertNotEqual(obj.updated_at, obj.created_at)
+        self.obj.save()
+        self.assertNotEqual(self.obj.updated_at, self.obj.created_at)
 
     def test_to_dict(self):
         """Asserts that the method to_dict() returns a dictionary from the
         __dict__ of an instance."""
 
-        obj = BaseModel()
-        dct = obj.to_dict()
-
+        dct = self.obj.to_dict()
         self.assertTrue(type(dct), dict)
 
     def test_to_dict_iso(self):
         """Asserts that the attributes 'created_at' and 'updated_at' are
         ISO formatted."""
 
-        obj = BaseModel()
-        dct = obj.to_dict()
-
+        dct = self.obj.to_dict()
         self.assertTrue(type(dct.get('created_at')), str)
         self.assertTrue(type(dct.get('updated_at')), str)
 
     def test_to_dict_has_class(self):
         """Asserts that the dictionary returned has the key '__class__'."""
 
-        obj = BaseModel()
-        dct = obj.to_dict()
-
+        dct = self.obj.to_dict()
         self.assertEqual(dct.get('__class__'), 'BaseModel')
 
+    def tearDown(self):
+        """tidy up after setUp method is called."""
 
-if __name__ == '__main__':
-    unittest.main()
+        FileStorage._FileStorage__objects = {}    # resets the attribute
+
+        fl = os.path.join(FileStorage._FileStorage__file_path,
+                          'file.json')    # path to the saved file
+        if os.path.isfile(fl) is True:
+            os.remove(fl)
