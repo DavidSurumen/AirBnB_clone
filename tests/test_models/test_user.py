5152007345403e0681fd3fa6d14c3b04ce502e39
David Surumen <davidsurumen@gmail.com>
2023-01-03 21:55:24 +0300

    tests for user module

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Test module for User."""
+import unittest
+from models.user import User
+from models.base_model import BaseModel
+
+
+class TestUser(unittest.TestCase):
+    """Test cases for the class User."""
+    
+    def setUp(self):
+        """sets up the test cases."""
+        self.obj = User()
+
+    def test_inheritance(self):
+        """Tests inheritance of class BaseModel."""
+        self.assertTrue(isinstance(self.obj, BaseModel))
+
+    def test_has_attributes(self):
+        """Tests the attributes of the User class."""
+
+        self.assertTrue(hasattr(self.obj, 'email'))
+        self.assertTrue(hasattr(self.obj, 'password'))
+        self.assertTrue(hasattr(self.obj, 'first_name'))
+        self.assertTrue(hasattr(self.obj, 'last_name'))
