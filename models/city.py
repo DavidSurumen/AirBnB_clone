586f03cd44da44d0dceb2d936c634c236c2a0a91
David Surumen <davidsurumen@gmail.com>
2023-01-05 22:08:15 +0300

    task 9 - city module

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""City class"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Blueprint for City objects."""
+    
+    state_id = ""    # will be State.id
+    name = ""
